727a3fa46df2dd68775a01e9e6476cad5f2b3fac
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-02-25 10:02:16 +0000

    Added average calculation program

---
+++ conditional/evenOdd.java
@@ -0,0 +1,15 @@
+package conditional;
+
+public class evenOdd {
+    public static void main (String args[])
+    {
+        int num = 23;
+        if  (num % 2 == 0){
+            System.out.println("EVEN NUMBER");
+        }
+        else{
+            System.out.println("ODD NUMBER");
+        }
+    }
+    
+}
---
+++ conditional/vote.java
@@ -0,0 +1,16 @@
+package conditional;
+
+//to check if can vote or not
+
+public class vote {
+    public static void main (String args[])
+    {
+        int age = 23;
+        if  (age >= 18){
+            System.out.println("Can VOTE!!");
+        }
+        else{
+            System.out.println("Can NOT vote");
+        }
+    }
+}
