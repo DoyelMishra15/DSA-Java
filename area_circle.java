2a32782800c9ccebe9cae972ee6482fa05df0e6c
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-02-25 09:50:00 +0000

    Added average calculation program

---
+++ area_circle.java
@@ -0,0 +1,8 @@
+public class area_circle {
+    public static void main(String args[])
+    {
+        int radius = 5;
+        double area = 3.14 * radius * radius;
+        System.out.println("Area is: " + area);
+    }
+}
