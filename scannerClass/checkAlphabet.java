e846ce51bdf27b3f691caab3dde714f37b07ef1b
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-02-27 09:02:39 +0000

    Added average calculation program

---
+++ scannerClass/checkAlphabet.java
@@ -0,0 +1,18 @@
+package scannerClass;
+
+import java.util.Scanner;
+
+public class checkAlphabet {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter characters:");
+        char x = scan.next().charAt(0);
+        if ((x>='A' && x<='Z')||(x>='a' && x<='z')){
+            System.err.println("It is an Alphabet");
+        }
+        else{
+            System.err.println("It is not an alphabet");
+        }
+        scan.close();
+}
+}
