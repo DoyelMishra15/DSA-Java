0ca8b2dfad763cbf045499c4996276cd3ad32125
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-02-28 09:01:21 +0000

    Added average calculation program

---
+++ forLoop/PerfectNumber.java
@@ -0,0 +1,31 @@
+package forLoop;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Program to check if a number is Perfect or Not");
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int sum = 0;
+
+        // Find sum of divisors
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        // Check if sum equals the number
+        if (sum == num && num > 0) {
+            System.out.println(num + " is a Perfect Number.");
+        } else {
+            System.out.println(num + " is NOT a Perfect Number.");
+        }
+
+        scanner.close();
+    }
+}
