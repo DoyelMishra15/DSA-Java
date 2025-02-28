351c8b2df0e3e9d370332284b13baa9aa75e10ec
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-02-28 09:51:56 +0000

    Added average calculation program

---
+++ whileLoop/CheckEven.java
@@ -0,0 +1,29 @@
+package whileLoop;
+import java.util.Scanner;
+
+public class CheckEven {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            int num = scan.nextInt();
+
+            if (num % 2 == 0) {
+                System.out.println(num + " is even.");
+            } else {
+                System.out.println(num + " is odd.");
+            }
+
+            System.out.print("Do you want to check another number? (y/n): ");
+            char ch = scan.next().charAt(0);
+
+            if (ch == 'n' || ch == 'N') {
+                break;
+            }
+        }
+
+        System.out.println("Exiting program.");
+        scan.close();
+    }
+}
