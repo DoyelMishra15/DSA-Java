e9ebdd5b78171e1eb0e6cd36a47fba6a9404de65
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-03-05 17:29:09 +0000

    Added average calculation program

--- rangeBased_prog/lastDig.java
+++ rangeBased_prog/lastDig.java
@@ -5,28 +5,38 @@ public class lastDig {
         int n = 321875;
 
         System.out.println("Program to Display the Last digit of given number");
-        System.err.println("last digit is : "+(n%10));
+        System.out.println("Last digit is: " + (n % 10));
 
         System.out.println("Program to Remove the Last digit of given number:");
-        System.err.println("Removing the last digit: " + (n/10));
+        System.out.println("Removing the last digit: " + (n / 10));
 
         System.out.println("Program to count the digits in given number");
         int num = n;
         int count1 = 0;
-        while (num!= 0) {
+        while (num != 0) {
             num /= 10;
-            count1++;            
+            count1++;
         }
-        System.out.println("Count is: "+ count1);
+        System.out.println("Count is: " + count1);
 
         System.out.println("Program to find the sum of digits in given number");
         num = n;
-        int sum1=0;
-        while(num>0){
-            int d1 = num%10;
-            sum1 += d1;
+        int d = 0; // Declaring 'd' properly
+        int sum1 = 0;
+        while (num > 0) {
+            d = num % 10;
+            sum1 += d;
             num /= 10;
         }
-        System.out.println("Sum is "+ sum1);
+        System.out.println("Sum is " + sum1);
+
+        // Checking if the last digit is even or odd
+        System.out.println("Program to check if the last digit is even or odd");
+        d = n % 10;
+        if (d % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
     }
 }
