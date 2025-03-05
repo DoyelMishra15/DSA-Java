a39b8cd4e73371a7bef95b36e409637b027a63b3
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-03-05 17:10:39 +0000

    Added average calculation program

---
+++ rangeBased_prog/lastDig.java
@@ -0,0 +1,10 @@
+package rangeBased_prog;
+
+public class lastDig {
+    public static void main(String[] args) {
+        int n = 321875;
+
+        //Program to Display the Last digit of given number
+        System.err.println("last digit is : "+(n%10));
+    }
+}
