1ce2a7ae85edb9499bf8141a5335a86e9902e8fe
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-03-05 17:15:22 +0000

    Added average calculation program

--- rangeBased_prog/lastDig.java
+++ rangeBased_prog/lastDig.java
@@ -10,6 +10,13 @@ public class lastDig {
         //Program to Remove the Last digit of given number:
         System.err.println("Removing the last digit: " + (n/10));
 
-        
+        //Program to count the digits in given number
+        int num = n;
+        int count1 = 0;
+        while (num!= 0) {
+            num /= 10;
+            count1++;            
+        }
+        System.out.println("Count is: "+ count1);
     }
 }
