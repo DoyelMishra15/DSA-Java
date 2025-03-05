b7f9a2bbb18db202f0358bb595f516d79519fa33
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-03-05 17:22:56 +0000

    Added average calculation program

--- rangeBased_prog/lastDig.java
+++ rangeBased_prog/lastDig.java
@@ -4,13 +4,13 @@ public class lastDig {
     public static void main(String[] args) {
         int n = 321875;
 
-        //Program to Display the Last digit of given number
+        System.out.println("Program to Display the Last digit of given number");
         System.err.println("last digit is : "+(n%10));
 
-        //Program to Remove the Last digit of given number:
+        System.out.println("Program to Remove the Last digit of given number:");
         System.err.println("Removing the last digit: " + (n/10));
 
-        //Program to count the digits in given number
+        System.out.println("Program to count the digits in given number");
         int num = n;
         int count1 = 0;
         while (num!= 0) {
@@ -18,5 +18,15 @@ public class lastDig {
             count1++;            
         }
         System.out.println("Count is: "+ count1);
+
+        System.out.println("Program to find the sum of digits in given number");
+        num = n;
+        int sum1=0;
+        while(num>0){
+            int d1 = num%10;
+            sum1 += d1;
+            num /= 10;
+        }
+        System.out.println("Sum is "+ sum1);
     }
 }
