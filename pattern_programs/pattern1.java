8806d6a5bab4a300563ed9167749c51755325181
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-03-06 11:28:24 +0000

    Added average calculation program

--- pattern_programs/pattern1.java
+++ pattern_programs/pattern1.java
@@ -17,5 +17,12 @@ public class pattern1 {
             }
             System.out.println();
         }
+        System.out.println();
+        for (int i=1 ; i<=5 ; i++){
+            for (int j=1 ; j<=5 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
