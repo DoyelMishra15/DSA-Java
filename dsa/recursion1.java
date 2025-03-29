eb0d22bd6f2b6f1416a9d0f2d17475632b4b2b4e
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2025-03-29 15:12:05 +0000

    Added

--- dsa/recursion1.java
+++ dsa/recursion1.java
@@ -3,12 +3,13 @@ package dsa;
 public class recursion1 {
     //stack overflow- A Stack Overflow occurs when a program's call stack 
     //exceeds its allocated memory limit, causing the program to crash. This typically happens due to infinite recursion or excessive function calls without returning.
-    void Prints(){
-        System.out.println("1");
-        Prints();
+    void Prints(int n){
+        if (n==0) return;
+        System.out.println(n);
+        Prints(n-1);
     }
     public static void main(String[] args) {
         recursion1 ps = new recursion1();
-        ps.Prints();
+        ps.Prints(5);
     }
 }
